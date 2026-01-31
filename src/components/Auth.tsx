49557e453de4b0fa2069d99f691c4d9967519db3
KarimKane1 <kane.mo@northeastern.edu>
2026-01-31 09:43:05 -0500

    Change email confirmation button to go back to sign in page

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -139,13 +139,14 @@ export function Auth() {
             type="button"
             onClick={() => {
               setEmailSent(false);
+              setIsSignUp(false); // Switch to sign in page
               setEmail('');
               setPassword('');
               setConfirmPassword('');
             }}
             className="w-full bg-blue-600 text-white py-3 rounded-xl font-semibold hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all"
           >
-            Back to Sign Up
+            Back to Sign In
           </button>
         </div>
       </div>
